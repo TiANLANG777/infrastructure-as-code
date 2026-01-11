4bd2cd50e4795691ff994f3e98625f2e40bf6463
Tianlang <147478458+TiANLANG777@users.noreply.github.com>
2026-01-11 19:48:42 +0800

    Update provider.tf

--- yandex_lab5/provider.tf
+++ yandex_lab5/provider.tf
@@ -7,13 +7,14 @@ terraform {
 }
 
 provider "yandex" {
-  # 这里的变量会从 Jenkins 环境变量中读取
+  # 这里的变量 yc_key_path 会由 Jenkins 注入，指向你的 authorized_key.json
   service_account_key_file = var.yc_key_path 
-  cloud_id                 = "b1g19f5102t4no376gjh"
-  folder_id                = "b1g1mbc29v77v98dkloi"
-  zone                     = "ru-central1-a"
+  cloud_id                 = "b1g19f5102t4no376gjh"  # 确认和你截图一致
+  folder_id                = "b1g1mbc29v77v98dkloi"  # 确认和你截图一致
+  zone                     = "ru-central1-a"         # 建议选这个区
 }
 
 variable "yc_key_path" {
-  type = string
+  type        = string
+  description = "Path to the authorized_key.json"
 }
