a2e400e632d563e122261a582902e9a7a470a3de
Tianlang <147478458+TiANLANG777@users.noreply.github.com>
2026-01-11 19:56:58 +0800

    Update main.tf

--- yandex_lab5/main.tf
+++ yandex_lab5/main.tf
@@ -1,3 +1,14 @@
+resource "yandex_vpc_network" "lab-net" {
+  name = "tianlang-net"
+}
+
+resource "yandex_vpc_subnet" "lab-subnet" {
+  name           = "tianlang-subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.lab-net.id
+  v4_cidr_blocks = ["10.128.0.0/24"]
+}
+
 resource "yandex_compute_instance" "vm-lab5" {
   name = "tianlang-yandex-vm"
 
@@ -8,24 +19,27 @@ resource "yandex_compute_instance" "vm-lab5" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd87tmbvcv1tdm9o2huv" # Ubuntu 20.04 LTS 镜像ID
+      # Yandex Cloud Ubuntu 20.04 LTS 镜像 ID
+      image_id = "fd87tmbvcv1tdm9o2huv" 
     }
   }
 
   network_interface {
     subnet_id = yandex_vpc_subnet.lab-subnet.id
-    nat       = true # 必须开启，否则 Jenkins 和 Ansible 连不上
+    nat       = true # 必须开启，否则 Jenkins 连不上
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}" # 替换为你的公钥内容
+    # 已经为你填好了公钥，用户名设为 ubuntu
+    ssh-keys = "ubuntu:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6jYQ9MkfYN5zM6xwXrfw9xBq/KlkBTqaJcXEa8TzRE0DuwNO7+2JONOLA8NWsuiF7VrYsiwqofMILr8s6Vd01gW+qvEiO96d/RCUB3LBy2n+52RmcE/Fm5VApRuFIeulP81aY/OZbHsIDO9EoX7mN/1APReNcXV0dESCaH4KC1Iur9nJwa9PYZv4HeIJq1vGAY5Z2lVMcyBf0Ego6YQrkmJsL4zfJ7Ynr9WUNwgWwR8Mum34xX4CBQi+Ej/NO5bKUVWC5F7LnjArET00WlfS8n1wfqoo2WXqybwU8car2eCccOPCU/PTs7f6KBMGX3XX2g47O0cq8QnE47kji061D ubuntu@lang-tian"
   }
 }
 
-resource "yandex_vpc_network" "lab-net" {}
+variable "yc_key_path" {
+  type        = string
+  description = "Path to the authorized_key.json provided by Jenkins"
+}
 
-resource "yandex_vpc_subnet" "lab-subnet" {
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.lab-net.id
-  v4_cidr_blocks = ["10.128.0.0/24"]
+output "instance_ip" {
+  value = yandex_compute_instance.vm-lab5.network_interface.0.nat_ip_address
 }
