c507ac7dd2abd6272d99d1d44e9ad97c8ce837fa
Tianlang <147478458+TiANLANG777@users.noreply.github.com>
2026-01-15 02:40:59 +0800

    commit

--- openstack/main.tf
+++ openstack/main.tf
@@ -6,20 +6,33 @@ terraform {
   }
 }
 
-# 这里的变量通常由 Jenkins 注入，或者使用默认值
-variable "image_name" { default = "ubuntu-22.04" } 
-variable "flavor_name" { default = "m1.medium" }
-variable "network_name" { default = "students-net" } # 注意检查这里是不是 students-net
-variable "key_pair_name" { default = "tianlang-key" }
-
-resource "openstack_compute_keypair_v2" "keypair" {
-  name       = "tianlang-auto-key"       # 改个名字确保不冲突
-  public_key = file("tianlang_key.pub")  # ✅ 正确：读取当前目录下自动生成的公钥
+# --- 1. 智能搜索镜像 ---
+data "openstack_images_image_v2" "ubuntu" {
+  # 正则表达式：查找名字里包含 "ubuntu" (不区分大小写) 的镜像
+  # 如果你的环境没有 22.04，它会自动找最新的 Ubuntu
+  name_regex  = "(?i)ubuntu" 
+  most_recent = true
+  visibility  = "public"
 }
 
+# --- 2. 变量定义 ---
+variable "flavor_name" { default = "m1.medium" }
+variable "network_name" { default = "students-net" } 
+variable "key_pair_name" { default = "tianlang-auto-key" }
+
+# --- 3. 创建密钥对 ---
+resource "openstack_compute_keypair_v2" "keypair" {
+  name       = var.key_pair_name
+  # 读取 Jenkins 现场生成的公钥
+  public_key = file("tianlang_key.pub") 
+}
+
+# --- 4. 创建虚拟机 ---
 resource "openstack_compute_instance_v2" "vm" {
   name            = "tianlang-bot-vm"
-  image_name      = var.image_name
+  # 使用搜索到的镜像 ID
+  image_id        = data.openstack_images_image_v2.ubuntu.id
+  
   flavor_name     = var.flavor_name
   key_pair        = openstack_compute_keypair_v2.keypair.name
   security_groups = ["default"]
@@ -29,6 +42,7 @@ resource "openstack_compute_instance_v2" "vm" {
   }
 }
 
+# --- 5. 输出 IP ---
 output "vm_ip" {
   value = openstack_compute_instance_v2.vm.access_ip_v4
 }
